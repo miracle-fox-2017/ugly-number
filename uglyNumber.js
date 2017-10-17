7d01553a4ebc78c03cddfb58d45007e9ec2cf6c8
yulianhp <kono.utsukushi24@gmail.com>
2017-10-17 11:49:32 +0700

    debugging ugly number done

--- uglyNumber.js
+++ uglyNumber.js
@@ -1,31 +1,43 @@
 'use strict'
 
-function maxDivides(number, divisible) {
+function maxDivide(number, divisible) {
+  debugger
   while(number % divisible == 0) {
+    debugger
     number = number / divisible;
   }
-  return numbers;
+  return number;
 }
 
 function isUgly(numberUgly) {
+  debugger
   numberUgly = maxDivide(numberUgly, 2);
   numberUgly = maxDivide(numberUgly, 3);
   numberUgly = maxDivide(numberUgly, 5);
 
-  return (numbers = 1) ? true : false;
+  if(numberUgly == 1) {
+    return true
+  } else {
+    return false
+  }
+
+  // return (numberUgly == 1) ? true : false;
 }
 
 function getUglyNo(input) {
+  debugger
   let i = 1;
-  let countUglyNumber == 1; // ugly number count
+  let countUglyNumber = 1; // ugly number count
 
   while(input > countUglyNumber) {
+    debugger
     i++;
     if(isUgly(i) == 1) {
-      counts++;
+      debugger
+      countUglyNumber++;
     }
-    return i;
   }
+    return i;
 }
 
 //driver code
